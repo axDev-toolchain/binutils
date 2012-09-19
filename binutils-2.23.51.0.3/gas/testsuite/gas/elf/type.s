6c0b3fe701e4ea1a70dd61174ce08d0dd4b6c714
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2012-09-19 08:56:14 +0159

    Add binutils 2.23.51.0.3
    Change-Id: Id52b52bf98c84979dedebb69efbbfb400b9851af
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

---
+++ binutils-2.23.51.0.3/gas/testsuite/gas/elf/type.s
@@ -0,0 +1,48 @@
+	.text
+	
+        .size   function,1
+        .type   function,%function
+function:
+	.byte	0x0
+	
+        .size   indirect_function,1
+        .type   indirect_function,%gnu_indirect_function
+indirect_function:
+	.byte	0x0
+	
+        .size   secondary_function,1
+	.secondary secondary_function
+        .type   secondary_function,%function
+secondary_function:
+	.byte	0x0
+	
+        .data
+	
+        .type   object,%object
+        .size   object,1
+object:
+	.byte	0x0
+        .type   tls_object,%tls_object
+	
+        .size   tls_object,1
+tls_object:
+	.byte	0x0
+        .type   notype,%notype
+	
+        .size   notype,1
+notype:
+	.byte	0x0
+
+	.type	unique_global,%gnu_unique_object
+unique_global:
+	.byte	0x0
+	.size	unique_global,1
+
+        .type   secondary_object,%object
+	.secondary secondary_object
+secondary_object:
+	.byte	0x0
+        .size   secondary_object,1
+
+	.comm	common, 1
+	.type   common,STT_COMMON
